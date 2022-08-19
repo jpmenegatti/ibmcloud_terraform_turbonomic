03049e6f2f8658618886d66a2f6276d4da034e5b
Joao Paulo Menegatti <jpmenegatti@gmail.com>
2022-08-19 18:50:40 -0300

    turbo v1

---
+++ main.tf
@@ -0,0 +1,71 @@
+resource "ibm_is_vpc" "turbonomic-vpc" {
+  name = "dal-${var.client_name}-vpc"
+  resource_group = "7091499c38984a33a077f69c422dfd1a"
+  default_network_acl_name = "dal-${var.client_name}-vpc-default-acl"
+  default_security_group_name = "dal-${var.client_name}-vpc-default-sg"
+  default_routing_table_name = "dal-${var.client_name}-route"
+}
+
+resource "ibm_is_subnet" "turbonomic-subnet" {
+  name            = "turbonomic-subnet"
+  vpc             = ibm_is_vpc.turbonomic-vpc.id
+  zone            = "us-south-1"
+  ipv4_cidr_block = "10.240.0.0/24"
+  resource_group = "7091499c38984a33a077f69c422dfd1a"
+}
+
+resource "ibm_is_instance" "turbonomic" {
+  name    = "${var.client_name}-turbonomic-instance"
+  image   = var.turbonomic_image_id
+  profile = "bx2-16x64"
+  metadata_service_enabled  = false
+  
+  boot_volume {
+    size = "210"
+  }
+  
+  primary_network_interface {
+    name = "eth0"
+    subnet = ibm_is_subnet.turbonomic-subnet.id
+	primary_ipv4_address = "10.240.0.6"
+    allow_ip_spoofing = true
+  }
+
+  vpc  = ibm_is_vpc.turbonomic-vpc.id
+  zone = "us-south-1"
+  keys = ["r006-1d3b8fca-3eda-4a63-9f3f-7a4dad8057dd"]
+  resource_group = "7091499c38984a33a077f69c422dfd1a"
+
+  //User can configure timeouts
+  timeouts {
+    create = "15m"
+    update = "15m"
+    delete = "15m"
+  }
+}
+
+resource "ibm_is_floating_ip" "turbonomic-floating-ip" {
+  name   = "turbonomic-floating-ip"
+  target = ibm_is_instance.turbonomic.primary_network_interface[0].id
+  resource_group = "7091499c38984a33a077f69c422dfd1a"
+}
+
+resource "ibm_is_instance_volume_attachment" "turbonomic-data" {
+  instance = ibm_is_instance.turbonomic.id
+
+  name                               = "turbonomic-data"
+  profile                            = "general-purpose"
+  capacity                           = "20"
+  delete_volume_on_attachment_delete = true
+  delete_volume_on_instance_delete   = true
+  volume_name                        = "turbonomic-data"
+  
+  resource_group = "7091499c38984a33a077f69c422dfd1a"
+
+  //User can configure timeouts
+  timeouts {
+    create = "15m"
+    update = "15m"
+    delete = "15m"
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,7 @@
+variable "turbonomic_image_id" {
+  default = "r006-47125f1b-b95d-47f5-8e4c-1395254549ce"
+}
+
+variable "client_name" {
+  default = "poc"
+}
